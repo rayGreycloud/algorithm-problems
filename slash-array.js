728049a19bbfdcf95409e08d0c9437893cb7790b
rayGreycloud <rayGreycloud@gmail.com>
2018-05-17 16:50:42 -0700

    Cut N items from array

---
+++ slash-array.js
@@ -0,0 +1,12 @@
+/*
+Return the remaining elements of an array after chopping off 
+n elements from the head.
+*/
+
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
+
+console.log(
+  slasher([1, 2, 3], 2);
+);
