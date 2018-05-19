b5d6b013c7c684b194d145e38e7f6f1aee10ca8b
rayGreycloud <rayGreycloud@gmail.com>
2018-05-19 14:04:16 -0700

    Convert special characters to html entities

---
+++ convert-to-html.js
@@ -0,0 +1,16 @@
+/*
+  Convert the characters &, <, >, ", ' in a string to 
+  their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+  return str.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
+console.log(
+  convertHTML("Dolce & Gabbana")
+);
