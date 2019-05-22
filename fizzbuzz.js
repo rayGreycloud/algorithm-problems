14834eebb804c0eebb1ec16e87642e80fabe857b
rayGreycloud <rayGreycloud@gmail.com>
2019-05-22 07:30:59 -0700

    Improved FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,20 @@
 // Original version
-for (i = 1; i < 11; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(i);
+function fizzBuzz1(limit) {
+  for (i = 1; i < 11; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
   }
 }
 
-// New version
-function fizzBuzz(limit) {
+// Shorter version
+function fizzBuzz2(limit) {
   for (i = 1; i <= limit; i++) {
     let count = '';
     count = i % 3 === 0 ? count + 'Fizz' : count;
@@ -21,4 +23,13 @@ function fizzBuzz(limit) {
   }
 }
 
-fizzBuzz(10);
+// Improved short version
+function fizzBuzz(limit) {
+  for (i = 1; i <= limit; i++) {
+    let count = i % 3 === 0 ? 'Fizz' : '';
+    count = i % 5 === 0 ? count + 'Buzz' : count;
+    console.log(count === '' ? i : count);
+  }
+}
+
+fizzBuzz(20);
